bac83dc2e6cc1dcae5588f4147031db6e1408d17
Roberto Ruffolo <robertoruffolo@hotmail.com>
2015-02-21 19:35:01 -0500

    Initial GUI implementation.

--- src/Briscola/Driver.java
+++ src/Briscola/Driver.java
@@ -1,9 +1,25 @@
 package Briscola;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Driver {
 
 	public static void main(String []args){
 		
 		System.out.println("Robby knows nothing at all, really.");
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				showGUI();
+			}
+		});
+	}
+	
+	public static void showGUI(){
+		JFrame frame = new JFrame("Briscola|McHacks 2015");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	
+		frame.setVisible(true);		
 	}
 }
