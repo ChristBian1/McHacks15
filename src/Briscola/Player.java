0554a1b8b2d2efaf1cac30fec7ffba1af7c6487f
Chris Bian <chris.a.bianchet@gmail.com>
2015-02-21 23:49:23 -0500

    Added Point System

--- src/Briscola/Player.java
+++ src/Briscola/Player.java
@@ -8,6 +8,7 @@ public class Player {
     
     private ArrayList<Card> hand= new ArrayList<Card>();
     private ArrayList<Card> cardsWon = new ArrayList<Card>();
+    private int points=0;
     
     public void setHand(Deck deck){
     	for(int i=0;i<3;i++)
