780c390948bcc315f4e9bbdf1f9c3634239b6980
Chris Bian <chris.a.bianchet@gmail.com>
2015-02-21 19:00:30 -0500

    Change Attempt

--- src/Briscola/Driver.java
+++ src/Briscola/Driver.java
@@ -4,6 +4,6 @@ public class Driver {
 
 	public static void main(String []args){
 		
-		System.out.println("Robby knows nothing at all.");
+		System.out.println("Robby knows nothing at all, really.");
 	}
 }
